bc2d2c364c804564969294692ae47f0ecdef9e9d
ParvathyAjRaj <paruvathy99@gmail.com>
2024-04-27 19:05:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Bargraph from './components/Bargraph/Bargraph';
 
 function App() {
   const api_key = process.env.REACT_APP_API_KEY;
+  console.log(api_key);
 
   const forecast_base_url = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}`;
   const days = 5;
